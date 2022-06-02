240b076b5f398ad72e575cedadc99c6795e69354
Maciej Wielgosz <wielgosz@agh.edu.pl>
2022-06-02 22:13:52 +0200

    per_joint loss - manual approach

--- src/pedestrians_video_2_carla/loss/per_joint_loc_2d.py
+++ src/pedestrians_video_2_carla/loss/per_joint_loc_2d.py
@@ -17,22 +17,10 @@ class PerJointLoc2DPoseLoss(Loc2DPoseLoss):
                         common_pred: Tensor,
                         common_gt: Tensor,
                         mask: Tensor = None) -> Tensor:
-        weights = self._weights[..., self._input_indices]
+        weights = self._weights[..., self._input_indices].unsqueeze(-1)
+        weights = weights * len(weights)
 
         if mask is not None:
-            return torch.sum(torch.tensor([
-                self._criterion(
-                    common_pred[..., i, :][mask[..., i]],
-                    common_gt[..., i, :][mask[..., i]],
-                ) * w
-                for i, w in enumerate(weights)
-                if torch.any(mask[..., i])
-            ], requires_grad=True))
+            return torch.sum(mask.unsqueeze(-1) * weights * (common_pred - common_gt) ** 2) / torch.numel(common_gt[mask])
 
-        return torch.sum(torch.tensor([
-            self._criterion(
-                common_pred[..., i, :],
-                common_gt[..., i, :],
-            ) * w
-            for i, w in enumerate(weights)
-        ], requires_grad=True))
+        return torch.sum(weights * (common_pred - common_gt) ** 2) / torch.numel(common_gt)
